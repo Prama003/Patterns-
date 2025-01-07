3cf041ba38953290c6b650afce80f188808634a7
Prama003 <151642281+Prama003@users.noreply.github.com>
2025-01-07 04:02:13 +0000

    Ternary operator

---
+++ Patterns.cpp/Ternary.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=-45;
+    cout<<(n>=0? "positive":"negative")<<endl;  //first operation is for true and second for false
+    return 0;
+}
